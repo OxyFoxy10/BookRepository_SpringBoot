b7ebc0b6b7a5b8a4cb38d219604abeb75414e2d7
OxyFoxy10 <oximuzyka@gmail.com>
2024-04-18 12:37:03 +0300

    application properties more than 100 characters style correction

---
+++ src/test/java/org/example/bookrepospringbootmate/mapper/CategoryMapperTest.java
@@ -0,0 +1,16 @@
+package org.example.bookrepospringbootmate.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryMapperTest {
+
+    @Test
+    void toDto() {
+    }
+
+    @Test
+    void toEntity() {
+    }
+}
